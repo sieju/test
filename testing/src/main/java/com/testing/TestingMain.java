a2ebd57efa8b44a959c93c0539810198966fa77c
sieju <sieju3@gmail.com>
2020-08-11 20:30:33 +0200

    Calculator TESTS

--- testing/src/main/java/com/testing/TestingMain.java
+++ testing/src/main/java/com/testing/TestingMain.java
@@ -1,8 +1,60 @@
 package com.testing;
 
+import com.testing.calculator.Calculator;
+
 public class TestingMain {
 	
 	public static void main(String[] args) {
 		System.out.println("Test modułu.");
+		
+		/*
+			TEST KALKULATORA
+		 */
+		
+		Calculator calc = new Calculator();
+		int addresult = calc.add(180, 19);
+		int subresult = calc.substract(200, 150);
+		int divresult = calc.division(50 , 5);
+		int multiplicationresult = calc.multiplication(5, 3);
+		int expresult = calc.exp(5);
+		
+		/*
+			TEST DODAWANIE I ODEJMOWANIA
+		 */
+		System.out.print("TEST DODAWANIA I ODEJMOWANIA:");
+		if (addresult == 199 && subresult == 50) {
+			System.out.println(" OK !");
+		} else {
+			System.out.println(" ERROR!");
+		}
+		
+		/*
+			TEST DZIELENIA , MNOZENIA, POTEGOWANIA
+		 */
+		
+		//EXPECTED RESULT 10
+		System.out.print("TEST DZIELENIA (SPODZIEWANE 10): " + divresult);
+		if (divresult == 10) {
+			System.out.println(" OK !");
+		} else {
+			System.out.println(" ERROR !");
+		}
+		
+		// EXPECTED RESULT 20
+		System.out.print("TEST MNOZENIA (SPODZIEWANE 20): " + multiplicationresult);
+		if (multiplicationresult == 20) {
+			System.out.println(" OK !");
+		} else {
+			System.out.println(" ERROR !");
+		}
+		
+		 //EXPECTED RESULT 25
+		System.out.print("TEST POTEGOWANIA (SPODZIEWANE 25): " + expresult);
+		if (expresult == 25) {
+			System.out.println(" OK !");
+		} else {
+			System.out.println(" ERROR !");
+		}
+		
 	}
 }
---
+++ testing/src/main/java/com/testing/calculator/Calculator.java
@@ -0,0 +1,31 @@
+package com.testing.calculator;
+
+public class Calculator {
+	int a ,b;
+	
+	public Calculator() {
+		this.a = a;
+		this.b = b;
+	}
+	
+	public int add(int a, int b){
+		return a+b;
+	}
+	
+	public int substract(int a, int b){
+		return a-b;
+	}
+	
+	public int exp(int a){
+		return a*a;
+	}
+	
+	public int division (int a, int b){
+		return a/b;
+	}
+	
+	
+	public int multiplication (int a, int b){
+		return a*b;
+	}
+}
